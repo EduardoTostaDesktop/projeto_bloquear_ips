e6a8c0c2d120693d80f01af6ee038978e45cd90a
EduardoTostaDesktop <eduardo.henrique@desktop.tec.br>
2025-08-29 11:58:23 -0300

    Links no detalhar lista

--- bloquearips/listas/urls.py
+++ bloquearips/listas/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('criar/', views.criar_lista, name='criar_lista'),            # Criar lista + upload CSV
     path('detalhar/<int:lista_id>/', views.detalhar_lista, name='detalhar_lista'),  # Detalhes da lista
     path('editar/<int:lista_id>/', views.editar_lista, name='editar_lista'),        # Editar lista
-    path('excluir/<int:lista_id>/', views.excluir_lista, name='excluir_lista'),
-    # ✅ URL para exclusão em massa
+    path('excluir/<int:lista_id>/', views.excluir_lista, name='excluir_lista'),# ✅ URL para exclusão em massa
     path('excluir-massa/', views.excluir_listas_massa, name='excluir_listas_massa'),
 ]
