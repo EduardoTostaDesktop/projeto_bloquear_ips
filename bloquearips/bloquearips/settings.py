eba3d07e0e828c19af641f084dee9a21529323cc
EduardoTostaDesktop <eduardo.henrique@desktop.tec.br>
2026-01-09 14:00:26 -0300

    Corrreção do settings para conexão com db

--- bloquearips/bloquearips/settings.py
+++ bloquearips/bloquearips/settings.py
@@ -4,9 +4,9 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-y#tky+5)x962isxcl6&^5j1x$0dnlwxpj9wx$n4!&(afpf--+8'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "projeto-bloquear-ips.onrender.com"]
 
@@ -57,23 +57,15 @@ WSGI_APPLICATION = 'bloquearips.wsgi.application'
 # ----------------------------
 # CONFIGURAÇÃO DO BANCO DE DADOS
 # ----------------------------
-if os.environ.get("RENDER") == "true":
+
     # Produção -> Usa PostgreSQL do Render
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=f"postgres://{os.environ['DB_USER']}:{os.environ['DB_PASSWORD']}@{os.environ['DB_HOST']}:{os.environ['DB_PORT']}/{os.environ['DB_NAME']}",
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    # Local -> Usa SQLite
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
