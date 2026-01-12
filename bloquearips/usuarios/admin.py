48263ef99b750f444aaa1e862e92d8c380e52ffe
EduardoTostaDesktop <eduardo.henrique@desktop.tec.br>
2026-01-12 08:36:01 -0300

    Adicionando permições de admin

--- bloquearips/usuarios/admin.py
+++ bloquearips/usuarios/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Usuario
 
-# Register your models here.
+
+@admin.register(Usuario)
+class UsuarioAdmin(UserAdmin):
+    model = Usuario
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        ("Informações pessoais", {"fields": ("first_name", "last_name", "email")}),
+        ("Permissões", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions",
+            )
+        }),
+        ("Datas importantes", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "username",
+                "password1",
+                "password2",
+                "is_staff",
+                "is_superuser",
+                "groups",
+            ),
+        }),
+    )
+
+    list_display = ("username", "email", "is_staff", "is_superuser")
+    search_fields = ("username", "email")
+    ordering = ("username",)
+
+    filter_horizontal = ("groups", "user_permissions")
