08d56525c50263a5b3cc2840bbf03de81ae4798c
EduardoTostaDesktop <eduardo.henrique@desktop.tec.br>
2026-01-13 09:07:04 -0300

    Modificações para uso local

--- bloquearips/bloquearips/settings.py
+++ bloquearips/bloquearips/settings.py
@@ -4,7 +4,11 @@ import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "django-insecure-dev-key"
+)
+
 
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
@@ -58,14 +62,24 @@ WSGI_APPLICATION = 'bloquearips.wsgi.application'
 # CONFIGURAÇÃO DO BANCO DE DADOS
 # ----------------------------
 
-    # Produção -> Usa PostgreSQL do Render
-DATABASES = {
-    "default": dj_database_url.config(
-        default=os.environ.get("DATABASE_URL"),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+if os.environ.get("DATABASE_URL"):
+    # Produção (Render)
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
+    }
+else:
+    # Desenvolvimento local (SQLite)
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
