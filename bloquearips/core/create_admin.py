58d3113b2b85e94393933e9bd20461f841c3f716
EduardoTostaDesktop <eduardo.henrique@desktop.tec.br>
2026-02-25 08:37:51 -0300

    Configuraçao para criação d euser admin

--- bloquearips/core/create_admin.py
+++ bloquearips/core/create_admin.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import get_user_model
 import os
+from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
@@ -9,7 +9,16 @@ password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
 if username and email and password:
     if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
-        print("Superuser criado com sucesso!")
+        user = User.objects.create_superuser(
+            username=username,
+            email=email,
+            password=password,
+        )
+
+        # 👇 ajuste o nome do campo se for diferente
+        user.tipo = "administrador"
+        user.save()
+
+        print("Superuser administrador criado com sucesso!")
     else:
         print("Superuser já existe.")
